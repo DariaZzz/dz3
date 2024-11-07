bb609adfb64743f0ab73563db951cda720f79c6f
Coder1 <coder1@example.ru>
2024-11-07 18:55:39 +0300

    works with comments

--- config/parser2.py
+++ config/parser2.py
@@ -1,10 +1,13 @@
 import re
-from importlib.resources import read_text
+# from importlib.resources import read_text
+import argparse
+from pathlib import Path
 
+import yaml
+
+comments = []
 dicts = []
 variables = {}
-service_words = ['dict(', ')', 'abs(', 'max(']
-dict_stack = []
 
 def parse(config_text):
     global variables
@@ -18,8 +21,10 @@ def parse(config_text):
     while symbol != '':
         symbol = config_text.read(1)
         if symbol == ';':
-            if re.match(r'\s*def\s*(.*?)\s*=\s*(.*?);', string + symbol, re.DOTALL):
-                variables.update(parse_definition(string + symbol))
+            if re.match(r'^\s*def\s*(.*?)\s*=\s*(.*?);$', string + symbol, re.DOTALL):
+                new_def = parse_definition(string + symbol)
+                dicts.append(dict({next(iter(new_def)): next(iter(new_def.values()))}))
+                variables.update(new_def)
                 string = ''
         elif symbol == ')':
             string += symbol
@@ -29,9 +34,34 @@ def parse(config_text):
                     string += symbol
                 except:
                     raise SyntaxError("Не хватает закрывающихся скобок")
-            if re.match(r'\s*dict\(\s*(.+?)\s*\)$', string + symbol, re.DOTALL):
-                dicts.append(parse_dict(string))
+            if re.match(r'^\s*dict\(\s*(.+?)\s*\)$', string + symbol, re.DOTALL):
+                new_d = parse_dict(string)
+                dicts.append(new_d)
+                variables.update(new_d)
                 string = ''
+        elif symbol == ']':
+            string += symbol
+            if re.match(r'\s*\^\[(.+?)]\s*$', string):
+                dicts.append(calculation(string))
+                string = ''
+        elif '||' in string:
+            while symbol != '\n':
+                string += symbol
+                symbol = config_text.read(1)
+            comment = re.findall(r'^\|\|\s*(.*?)\s*$', string, re.DOTALL)[0]
+            comments.append(comment)
+            string = ''
+        elif '{%' in string:
+            while '%}' not in string:
+                string += symbol
+                symbol = config_text.read(1)
+                if symbol == '':
+                    raise SyntaxError("Комментарий не закрыт")
+            comment = re.findall(r'^\s*\{\%\s*(.*?)\s*\%\}$', string, re.DOTALL)[0]
+            list_of_comments = comment.split('\n')
+            for comm in list_of_comments:
+                comments.append(comm)
+            string = ''
         else:
             string += symbol
     return variables, dicts
@@ -51,7 +81,9 @@ def parse_dict(text):
     while config_text != '':
         symbol = config_text[0]
         config_text = config_text[1:]
-        if symbol == ',' or config_text == '':
+        if symbol == ',' or config_text == '' and string.count('dict') == 0:
+            if 'this' in string:
+                print()
             if symbol != ',':
                 string += symbol
             if string.count('dict') == 0 and re.match(r'\s*(.+?)\s*=\s*(.+?)', string, re.DOTALL):
@@ -61,7 +93,7 @@ def parse_dict(text):
                 string = ''
             elif symbol == ',':
                 string += symbol
-        elif symbol == ')':
+        elif symbol == ')' or config_text == '':
             string += symbol
             while config_text != '' and string.count('(') != string.count(')'):
                 try:
@@ -73,10 +105,10 @@ def parse_dict(text):
                     raise SyntaxError("Не хватает закрывающихся скобок")
             if re.match(r'\s*(.+?)\s*=\s*dict\((.+?)\)$', string, re.DOTALL):
                 name = re.findall(r'[a-zA-Z_]\w+', string, re.DOTALL)[0]
-                if name == 'inner_item2':
-                    print()
                 new_dict = re.findall(r'\s*[a-zA-Z_]\w+\s*=\s*(.+?)$', string, re.DOTALL)[0]
-                result[name] = parse_dict(new_dict)
+                parsed_dict = parse_dict(new_dict)
+                variables[name] = parsed_dict
+                result[name] = parsed_dict
                 string = ''
         else:
             string += symbol
@@ -128,19 +160,78 @@ def parse_assignment(definition):
 
     matches = re.findall(pattern, definition)
     for name, value in matches:
-        if not is_valid_variable_name(name.strip()):
-            raise SyntaxError(f"Invalid variable name: {name.strip()}")
+        if name == 'my_var':
+            print()
         result[name.strip()] = parse_value(value.strip())
     return result
 
+def calculation(text):
+    pattern = r'\s*\^\[(.+?)\]\s*$'
+    compute = re.findall(pattern, text, re.DOTALL)[0]
 
+    pattern1 = r'^\s*(.+?)\s*(.+?)\s*\+\s*$'
+    pattern2 = r'^\s*(.+?)\s*(.+?)\s*max\s*$'
+    pattern3 = r'^\s*(.+?)\s*abs\s*$'
 
-# Пример использования:
-file = open('example.txt', 'r')
-# config_text = file.read()
+    if re.match(pattern1, compute, re.DOTALL):
+        operation = '+'
+    elif re.match(pattern2, compute, re.DOTALL):
+        operation = 'max'
+    elif re.match(pattern3, compute, re.DOTALL):
+        operation = 'abs'
+    else:
+        raise SyntaxError(f"Неправильный формат операции {compute}")
+    my_list = compute.split()
+    res = ''
+    match operation:
+        case '+':
+            operand1 = my_list[0]
+            operand2 = my_list[1]
+            num1 = parse_value(operand1)
+            num2 = parse_value(operand2)
+            res = num1+num2
+        case 'max':
+            operand1 = my_list[0]
+            operand2 = my_list[1]
+            num1 = parse_value(operand1)
+            num2 = parse_value(operand2)
+            res = max(num1, num2)
+        case 'abs':
+            operand = my_list[0]
+            num = parse_value(operand)
+            res = abs(num)
+    return res
 
-try:
-    result = parse(file)
-    print(result)
-except SyntaxError as e:
-    print(f"Syntax error: {e}")
+# # # Пример использования:
+# file = open('example.txt', 'r', encoding='utf8')
+# # config_text = file.read()
+#
+# try:
+#     result = parse(file)
+#     print(result[1])
+#     yaml_data = yaml.dump(result[1], allow_unicode=True, default_flow_style=False)
+#     print(yaml_data)
+# except SyntaxError as e:
+#     print(f"Syntax error: {e}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Парсинг файла конфигурации.")
+    parser.add_argument("filepath", type=str, help="Путь до файла конфигурации")
+
+    args = parser.parse_args()
+    file_path = Path(args.filepath)
+
+    # Проверка существования файла
+    if not file_path.is_file():
+        print(f"Файл {file_path} не найден.")
+    else:
+        with file_path.open("r", encoding='utf8') as file:
+            try:
+                result = parse(file)
+                # print(result[1])
+                yaml_data = yaml.dump(result[1], allow_unicode=True, default_flow_style=False)
+                for comment in comments[::-1]:
+                    yaml_data = f'# {comment} \n' + yaml_data
+                print(yaml_data)
+            except SyntaxError as e:
+                print(f"Ошибка синтаксиса: {e}")
